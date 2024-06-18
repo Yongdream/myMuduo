ab99736b56d7b6ea916549ab7c57a93e3ed28dc4
yongdream <64509924+Yongdream@users.noreply.github.com>
2024-06-18 23:07:50 +0800

    noncopyable

---
+++ noncopyable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * noncopyable被继承以后  派生类对象可以进行正常的构造&析构
+ * 但是派生类无法进行拷贝和赋值操作
+*/
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator = (const noncopyable) = delete;
+private:
+    noncopyable() = default;
+    ~noncopyable() = default;
+};
